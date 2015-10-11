ed781c8c4e5d04126298cf2f08f13c756ed19ca7
David Rothblatt <daroth13@gmail.com>
2015-10-11 19:18:38 -0400

    basics for login page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session
 
 import sqlite3
+import auth
 
 app = Flask(__name__)
 
@@ -12,8 +13,28 @@ def home():
 
 @app.route("/login")
 @app.route("/login/")
+
+@app.route("/login", methods=["GET","POST"])
+@app.route("/login/", methods=["GET","POST"])
+
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        uname = request.form['username']
+        pword = request.form['password']
+        button = request.form['button']
+
+        if button=="cancel":
+            return render_template('login.html')
+
+        if auth.authenticate(uname, pword):
+            if 'username' not in session:
+                session['username'] = uname
+                return redirect(url_for("secret"))
+        else:
+            err = "INVALID USERNAME OR PASSWORD!!"
+            return render_template("login.html", err = err)
 
 
 if __name__ == "__main__":
