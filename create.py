79f9228d2fa64349c88dee4698e12ddcdcb499b3
David Rothblatt <daroth13@gmail.com>
2015-10-13 19:25:44 -0400

    testing db functionality

--- create.py
+++ create.py
@@ -1,24 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db")
+def create(): 
+	conn = sqlite3.connect("database.db")
 
-c = conn.cursor()
+	c = conn.cursor()
 
-q = "create table users(username text, password text, u_id integer, name text) if does not exist"
-c.execute(q)
+	q = "create table users(username text, password text, u_id integer, name text) if does not exist"
+	c.execute(q)
 
-q = "insert into users values ("drothblatt", "cronut123", 2589, "David Rothblatt");"
-c.execute(q)
+	q = "insert into users values("drothblatt", "cronut123", 2589, "David Rothblatt");"
+	c.execute(q)
 
-q = "create table posts(body text, title text, username text, post_id integer) if does not exist"
-c.execute(q)
+	q = "create table posts(body text, title text, username text, post_id integer) if does not exist"
+	c.execute(q)
 
-q = "insert into posts values ("Hi there", "First Post", "drothblatt", 1);" 
-c.execute(q)
+	q = "insert into posts values ("Hi there", "First Post", "drothblatt", 1);" 
+	c.execute(q)
 
-q = "create table comments(post_id integer, u_id integer, comment text, comment_time date) if does not exist"
-c.execute(q)
+	q = "create table comments(post_id integer, u_id integer, comment text) if does not exist"
+	c.execute(q)
 
-conn.commit()
+	conn.commit()
 
 
