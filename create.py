a10c484044bc2904f488e3ef6ce25be0cc975cf7
David Rothblatt <daroth13@gmail.com>
2015-10-13 19:03:50 -0400

    working w db stuff

--- create.py
+++ create.py
@@ -4,6 +4,13 @@ conn = sqlite3.connect("database.db")
 
 c = conn.cursor()
 
-q = "create table User(User_ID integer, Name text) if does not exist"
+q = "create table users(username text, password text, u_id integer, name text) if does not exist"
 c.execute(q)
 
+q = "create table posts(body text, title text, username text, post_id integer, post_time date) if does not exist"
+c.execute(q)
+
+q = "create table comments(post_id integer, u_id integer, comment text, comment_time date) if does not exist"
+c.execute(q)
+
+conn.commit()
