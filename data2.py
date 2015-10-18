fd85b098d16f67c62cb20d040407edbeb46ece8a
MGRiv <mgrivera501@gmail.com>
2015-10-18 00:29:32 -0400

    solving database issues

--- data2.py
+++ data2.py
@@ -3,9 +3,7 @@ import sqlite3
 def make_table():
 	con = sqlite3.connect("database.db")
 	c = con.cursor()
-
 	q = "create table if not exists users(uname text, pword text, userid integer, rname text)"
-	q = "create table if not exists users(uname text, pword text, userid uname)"
         c.execute(q)
         q = "create table if not exists post(words text, postid integer, authorid integer, date text)"
         c.execute(q)
@@ -43,7 +41,12 @@ def delete_user(uname, pword):
     c.exeucte(q)
     con.commit()
 
-
+def all_user():
+        q = " SELECT * FROM users"
+        result = c.execute(q)
+        for r in result:
+                print r
+                print"\n"
 #------------------------------COMMENT TABLES-----------------------------------
 def add_comment(words, postid, commid, authorid, date):
 	con = sqlite3.connect("database.db")
@@ -74,6 +77,13 @@ def delete_comment(authorid,commid):
         q = "UPDATE comment SET words = '{}' WHERE authorid = '{}' AND postid = '{}'".format("This comment is no longer viewable",authorid,commid)
         c.execute(q)
     con.commit()
+
+def all_comment():
+        q = " SELECT * FROM comment"
+        result = c.execute(q)
+        for r in result:
+                print r
+                print"\n"
             
 #-------------------------------POST TABLES--------------------------------------
 def add_post(words, postid, authorid,date):
@@ -116,18 +126,30 @@ def delete_post(postid,authorid):
                 print r
                 print"\n"
 
+def all_post():
+        q = " SELECT * FROM post"
+        result = c.execute(q)
+        for r in result:
+                print r
+                print"\n"
+#---------------------------------------------------------------------------------------------
+
 con = sqlite3.connect("database.db")
 c = con.cursor()
-c.execute("delete from post")
+c.execute("DELETE FROM post")
+c.execute("DELETE FROM users")
+c.execute("DELETE FROM comment")
 con.commit()
 make_table()
 add_post("alsdfughlkasdbggfadgr","1","authorid","1/1/11")
 delete_post(":)","authorid")
 delete_post("1","dsagouhadfkg")
 delete_post("1","authorid")
-
-print("------------------------------------")
-add_user("greg","gerg",2271, "Gregory Redozubov")
-
-print("-------------------------------")
 add_comment("HIYA GUYS", 123, 456, 789, "10/16/15")
+add_user("greg","gerg",2271, "Gregory Redozubov")
+print("--------------------------------")
+all_user()
+print("-----------------------------------")
+all_post()
+print("-----------------------------------")
+all_comment()
